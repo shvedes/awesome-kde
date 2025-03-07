d4587c69880585e8a8154abc58998623ed38b733
shvedes <shved.personal@gmail.com>
2025-03-07 12:30:26 +0100

    kio-servicemenus: install script is complete now

--- extras/kio-servicemenus/install.sh
+++ extras/kio-servicemenus/install.sh
@@ -2,7 +2,13 @@
 
 export XDG_DATA_HOME="$HOME/.local/share"
 
-dependencies=("kdialog" "magick" "git")
+convert_image_script_url="https://raw.githubusercontent.com/shvedes/awesome-kde/refs/heads/main/extras/kio-servicemenus/scripts/convert-image.sh"
+convert_image_servicemenus_url="https://raw.githubusercontent.com/shvedes/awesome-kde/refs/heads/main/extras/kio-servicemenus/servicemenus/convert-image.desktop"
+
+scripts_dir="$HOME/.local/bin/kio-servicemenus"
+servicemenus_dir="$XDG_DATA_HOME/kio/servicemenus"
+
+dependencies=("kdialog" "magick" "git" "wget")
 missing_deps=()
 
 for item in "${dependencies[@]}"; do
@@ -17,7 +23,16 @@ if [[ -n "${missing_deps[*]}" ]]; then
 fi
 
 echo "==> Creating directories"
-# mkdir -p "$XDG_DATA_HOME/kio/servicemenus"
-# mkdir -p "$HOME/.local/bin/kio-servicemenus"
+mkdir -p "$XDG_DATA_HOME/kio/servicemenus"
+mkdir -p "$HOME/.local/bin/kio-servicemenus"
 
 echo "==> Copying files"
+wget -q "$convert_image_script_url" -O "$scripts_dir/convert-image.sh"
+wget -q "$convert_image_servicemenus_url" -O "$servicemenus_dir/convert-image.desktop"
+
+echo "==> Setting things up"
+chmod +x "$scripts_dir/convert-image.sh"
+chmod +x "$servicemenus_dir/convert-image.desktop"
+sed -i "s/username/$(whoami)/" "$servicemenus_dir/convert-image.desktop"
+
+echo "==> Done"
